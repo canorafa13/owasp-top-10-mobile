ab810def5da8aff0f6e25feddfba886cd8818762
Rafael Cano Martinez <canorafa13@gmail.com>
2025-04-22 02:28:38 -0600

    update: rename api folder

--- backend-app-owasp-demo-nodejs/api/core/plugins/users/handlers.js
+++ backend-app-owasp-demo-nodejs/api/core/plugins/users/handlers.js
@@ -26,22 +26,24 @@
             const methods = req.server.methods;
             const payload = req.pre.payload || req.payload;
 
-            /// Validar que el password sea valido
-            let schema = new PasswordValidator();
+            if(isSecure){
+                /// Validar que el password sea valido
+                let schema = new PasswordValidator();
 
-            schema
-                .is().min(8)                                    // Minimum length 8
-                .is().max(30)                                  // Maximum length 30
-                .has().uppercase(1)                              // Must have uppercase letters
-                .has().lowercase(1)                              // Must have lowercase letters
-                .has().digits(1)                                // Must have at least 1 digits
-                .has().not().spaces()                           // Should not have spaces
-                .has().symbols(1)
-                .is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
+                schema
+                    .is().min(8)                                    // Minimum length 8
+                    .is().max(30)                                  // Maximum length 30
+                    .has().uppercase(1)                              // Must have uppercase letters
+                    .has().lowercase(1)                              // Must have lowercase letters
+                    .has().digits(1)                                // Must have at least 1 digits
+                    .has().not().spaces()                           // Should not have spaces
+                    .has().symbols(1)
+                    .is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
 
-            let isValidPassword = schema.validate(payload.password, { details: true });
-            if(isValidPassword.length != 0){
-                return Boom.badRequest(JSON.stringify(isValidPassword));
+                let isValidPassword = schema.validate(payload.password, { details: true });
+                if(isValidPassword.length != 0){
+                    return Boom.badRequest(JSON.stringify(isValidPassword));
+                }
             }
 
             payload.rol_id = 3;
